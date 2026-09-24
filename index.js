5dac79903e7f1d6a75443ac3617f5d57e6b87d3f
Gin-356A <Gin_356A@proton.me>
2026-09-24 18:45:51 +0530

    Added all functionality

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-let color = null
 
-document.addEventListener("change" , function(){
-    color = document.getElementById("color-selected").value
-    fetchColor()
+
+
+document.addEventListener("click", function(e){
+    if (e.target.className === "btn"){
+        const color = document.getElementById("color-selected").value.substring(1)
+
+        genrateColorScheme(color)
+    }
 })
 
-function fetchColor(){
-    fetch(`https://www.thecolorapi.com/id?hex=#${color}&format=JSON`)
-    .then(response => response.json())
-    .then(data => console.log(data))
+function genrateColorScheme(color){
+    const mode = document.getElementById("mode").value
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}&count=5`)
+        .then(response => response.json())
+        .then(data => {
+            const colorArray = data.colors.map( clr => {
+                return {
+                    hex: clr.hex.clean,
+                    image : clr.image.bare
+                }
+            })
+            console.log(colorArray)
+            renderColorScheme(colorArray)
+        })    
 }
 
+function renderColorScheme(colorArray) {
 
-// function renderColor(){
-    
-//     fetch("https://www.thecolorapi.com/scheme")
-// }
+    const colorList = document.querySelector(".color-list")
+
+    colorList.innerHTML = ""
+
+    for (let i = 0; i < colorArray.length; i++) {
+        colorList.innerHTML += `
+            <li class="color-item">
+                <img
+                    class="color-image"
+                    src="${colorArray[i].image}"
+                    alt="Color scheme preview"
+                >
+                <p class="hex-value">#${colorArray[i].hex}</p>
+            </li>
+        `
+    }
+}
